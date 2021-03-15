4b046cd26e5e551f0b2629ebbe4bf6941d161b70
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-03-15 03:23:06 -0700

    add setTimeout function

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -5,7 +5,12 @@ const message = document.getElementById('redirect');
 function redirect(event) {
     event.target.closest('body').style.backgroundColor = 'gray';
     event.target.closest('body').style.opacity = '40%';
-    alert('Thank you for your business! :)');
+    setTimeout(function() {
+        if (confirm('Thank you for your business! :)')) {
+            event.target.closest('body').style.backgroundColor = 'none';
+            event.target.closest('body').style.opacity = '100%';
+        }
+    }, 1000);
 };
 
 order.addEventListener('click', redirect);
