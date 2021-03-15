4cf4efc0b167a0662a63c176633710589fd79342
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-03-15 01:43:59 -0700

    add event handler and event listener to order

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,2 +1,12 @@
-const order = document.getElementById('order');
-const book = document.getElementById('book');
+const order = document.getElementById('order_one');
+const book = document.getElementById('book_one');
+
+function redirect(event) {
+    event.target.closest('body').innerHTML =
+        `<div id="redirect">
+          <p>Thank you for your business! :)<p>
+          <button type="button" class="btn btn-outline-danger">Exit</button>
+        </div>`;
+};
+
+order.addEventListener('click', redirect);
