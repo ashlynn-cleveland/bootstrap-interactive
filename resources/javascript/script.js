f039e2f2929794b9f94a74cd9c06452208282b61
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-03-15 02:41:00 -0700

    change redirect ruleset

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,9 +1,11 @@
 const order = document.getElementById('order_one');
 const book = document.getElementById('book_one');
+const message = document.getElementById('redirect');
 
 function redirect(event) {
     event.target.closest('body').style.backgroundColor = 'gray';
-    event.target.closest('body').style.opacity = '40%';
+    event.target.closest('body').style.opacity = '40%'
+    message.style.display = 'visible';
 };
 
 order.addEventListener('click', redirect);
