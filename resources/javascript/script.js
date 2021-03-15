feef7083d6a1138ccd8cce6b9c7c03b7a5ea2b0c
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-03-14 23:37:22 -0700

    add welcome, menu, and redirect variables

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,3 +1,8 @@
 const order = document.getElementById("order");
 const book = document.getElementById("book");
+const welcome = document.getElementById("intro-para");
+const menu = document.getElementById("menu");
 
+const redirect = function() {
+
+}
