2440ef33a3d116a0784c379890dc675fe0dec99c
Ashlynn Cleveland <75820528+ashlynn-cleveland@users.noreply.github.com>
2021-03-14 23:53:15 -0700

    fill in redirect function expression

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,8 +1,21 @@
-const order = document.getElementById("order");
-const book = document.getElementById("book");
-const welcome = document.getElementById("intro-para");
-const menu = document.getElementById("menu");
+const order = document.getElementById('order');
+const book = document.getElementById('book');
+const welcome = document.getElementById('intro-para');
+const menu = document.getElementById('menu');
 
 const redirect = function() {
-
-}
+    if (event.target.closest('intro-para') === true) {
+        welcome.innerHTML =
+            `<div id="redirect">
+          <p>Thank you for your business!:)</p>
+          <button class="btn btn-outline-danger">Exit</button>
+         </div>`;
+    } else if (event.target.closest('menu') === true) {
+        menu.innerHTML =
+            `<div id="redirect">
+          <p>Thank you for your business!:)</p>
+          <button class="btn btn-outline-danger">Exit</button>
+         </div>`;
+    };
+    console.log('I ran!');
+};
